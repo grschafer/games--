2212447a159401c72868be8685d54f6d4ee07544
Christopher Johnson <cjohnson1021@huskers.unl.edu>
2012-11-10 16:20:17 -0600

    Physics working

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -2,17 +2,18 @@ Crafty.scene("main", function() {
 
 	var elements = [
         "src/entities/ufo.js",
-        "src/interfaces/info.js",
 		"src/entities/floor.js",
-		"src/entities/enemy.js"
+		"src/entities/enemy.js",
+		"src/entities/box.js"
 	];
 	
 	//when everything is loaded, run the main scene
 	require(elements, function() {	   
 		sc['ufo'] = new Ufo();
 		sc['enemy'] = new Enemy();
-		infc['info'] = new Info();
 		sc['floor'] = new Floor();
 		sc['key'] = new Key();
+		sc['box'] = new Box();
+		sc['boulder'] = new Boulder();
 	});
 });
