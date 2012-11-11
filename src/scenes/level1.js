b0728d85de9c3d08c3516ef124a3317fb177c9fd
jcox92 <joshua.ms.cox@gmail.com>
2012-11-10 19:02:20 -0800

    dasflkds

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -1,14 +1,35 @@
 Crafty.scene("level1", function() {
 
 	var elements = [
-        "src/entities/floor.js",
-        "src/entities/box.js"
+        "src/entities/ufo.js",
+		"src/entities/floor.js",
+		"src/entities/cactus.js",
+		"src/entities/wires.js",
+		"src/entities/box.js",
+		"src/entities/platform.js"
 	];
 	
 	//when everything is loaded, run the main scene
 	require(elements, function() {	   
-		sc['floor'] = new Floor();
-		sc['box'] = new Box();
+		var bg = Crafty.e("2D, Canvas, Image")
+             .attr({w: Crafty.viewport.width, h: Crafty.viewport.height, z:0})
+             .image("web/images/Background1.jpg", "repeat");
+		sc['ufo'] = new Ufo(0, 0.001, {}, 'level1', 'win');
+		sc['ufo'].getEntity().attr({x: 200, y:500});
+		sc['p4'] = new Platform(0, 0, 580, 800, 50, 2);
+		sc['key'] = new Key();
+		sc['cactus'] = new Cactus();
+		sc['cactus'].getEntity().attr({x: 700, y: 400});
+		sc['wires'] = new Wires();
+		sc['wires'].getEntity().attr({x: 350, y:0});
+		//sc['floor'] = new Floor();
+		sc['key'] = new Key(0, 500, 500, "LeftArrow");
+		sc['rightkey'] = new Key(0, 200, 350, "RightArrow");
+		sc['upkey'] = new Key(0, 50, 350, "UpArrow");
+		sc['door'] = new Door(0, 500, 150);
+		sc['p1'] = new Platform(0,50, 500, 100, 50, 1);
+		sc['p2'] = new Platform(0,650, 500, 100, 50, 1);
+		sc['p3'] = new Platform(0,150, 300, 500, 50, 1);
 	});
 
 });
---
+++ src/scenes/win.js
@@ -0,0 +1,34 @@
+Crafty.scene("win", function() {
+
+//	var elements = [
+//        "src/entities/ufo.js",
+//		"src/entities/floor.js",
+//		"src/entities/cactus.js",
+//		"src/entities/wires.js",
+//		"src/entities/box.js",
+//		"src/entities/platform.js"
+//	];
+	
+	//when everything is loaded, run the main scene
+//	require(elements, function() {
+
+		
+
+		var bg = Crafty.e("2D, Canvas, Image, Keyboard")
+             .attr({w: Crafty.viewport.width, h: Crafty.viewport.height, z:0})
+             .image("web/images/EndScreen.jpg", "repeat");
+//		sc['ufo'] = new Ufo();
+//		sc['floor'] = new Floor();
+//		sc['key'] = new Key();
+//		sc['cactus'] = new Cactus();
+//		sc['wires'] = new Wires();
+//		sc['floor'] = new Floor();
+//		sc['key'] = new Key();
+//		sc['box'] = new Box();
+//		sc['boulder'] = new Boulder();
+//		sc['p1'] = new Platform(0,50, 500, 100, 60);
+		
+//		Crafty.background("../../web/images/title_screen.jpg");
+		
+//	});
+});
